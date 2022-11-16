1d0cb0f4009b22a4db9be5de6a048bacdf5c2ea2
WinterGud <92243101+WinterGud@users.noreply.github.com>
2022-11-16 09:02:55 +0300

    Update Pr-8_Ex-0.cpp

--- Practic/Pr-8_Ex-0.cpp
+++ Practic/Pr-8_Ex-0.cpp
@@ -1,1 +1,30 @@
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+int main() {
+
+    int length;
+    cout << "Ввeдіть розмірність масиву: ";
+    cin >> length;
+    int* Arr = new int[length];
+    int k = length / 2;
+    for (int i = 0; i < length; i++)
+    {
+        if (i < length / 2)
+        {
+            Arr[i] = i + 1;
+            cout << Arr[i] << endl;
+        }
+        else if (i >= +length / 2)
+        {
+            Arr[i] = k--;
+            cout << Arr[i] << endl;
+        }
+    }
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
