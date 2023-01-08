2b9c2440447d06e214a7d20886e95480a2153799
WinterGud <92243101+WinterGud@users.noreply.github.com>
2023-01-08 23:30:48 +0300

    Create newArray.cpp

---
+++ Practic/arrive/newArray.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+
+    int num; std::cin >> num;
+    num = 1000;
+    double* arr = new double[num];
+    for (int i = 0; i < num ; i++)
+    {
+        arr[i] = rand();
+    }
+     
+    double* arrNext = new double[num];
+    for (int i = 0; i < num; i++)
+    {
+        arrNext[i] = arr[i] + arr[i + 2];
+    }
+    //я незнаю як воно запустилось -_-
+
+    int k; std::cin >> k;
+    for (int i = 0; i < num; i++)
+    {
+        if (arrNext[i] > k) {
+            std::cout << arrNext[i] << ' ' << i << std::endl;
+        }
+    }
+
+    
+
+
+
+    return 0;
+}
